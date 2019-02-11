f7f1a57a3e848d9645c39e2b8b3bc60f53ded246
Luke Uritescu <Luritescu@gmail.com>
2019-02-11 13:56:09 -0600

    update tto unit test Airplane

--- UnitTestFlyingVehicle/UnitTestAirplane.cs
+++ UnitTestFlyingVehicle/UnitTestAirplane.cs
@@ -1,15 +1,86 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using OOP2;
 namespace UnitTestFlyingVehicle
 {
     [TestClass]
     public class UnitTestAirplane
     {
-        
+        Airplane ap;
         [TestMethod]
-        public void TestMethod1()
+        public void TestAirPlaneAabout()
         {
+            //Arrange
+            ap = new Airplane();
+            //Act
+            string originalAirplaneAboutString = ap.About();
+            ap.StartEngine();
+            string engineStartAirPlaneString = ap.About();
+            ap.StopEngine();
+            string engineStopAirPlaneString = ap.About();
+            //Assert
+            Assert.AreEqual(originalAirplaneAboutString, engineStopAirPlaneString);
+            Assert.AreNotEqual(originalAirplaneAboutString, engineStartAirPlaneString);
+        }
+
+        [TestMethod]
+        public void TestAirPlaneConstructor()
+        {
+            //Arrange
+            ap = new Airplane();
+            //Act
+            //Assert
+            Assert.AreEqual(0, ap.CurrentAltitude);
+            Assert.AreEqual(41000, ap.MaxAltitude);
+            Assert.AreEqual(false, ap.IsFlying);
+        }
+
+        [TestMethod]
+        public void TestAirPlaneFlyDownWhileEngineOff()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneFlyDownWhileEngineOn()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneFlyUpWhileEngineOff()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneFlyUpWhileEngineOn()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneGetEngineString()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneStartEngine()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneStopEngine()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAirPlaneTakeOff()
+        {
+
         }
     }
 }
